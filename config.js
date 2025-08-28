7da0a579566d4a3216ed13d7f398da48a2938130
ZynXx <techigwe@gmail.com>
2025-08-28 18:19:16 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteName: "Parfaite Shop",
+  brandTagline: "Tout ce que vous voulez, en un clic",
+  companySignature: "by David Tech Inc – Compte certifié",
+  whatsappNumber: "24162385777",
+  whatsappGroupUrl: "https://chat.whatsapp.com/XXXXXXXXXXXXXXX", // Remplacer
+  adminDefault: { username: "parfaite", password: "backmanoffcl" }
+};
